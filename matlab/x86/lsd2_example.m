ce09a7504d918366314202e8e207e5c8049c13e0
primetang <tanggefu@gmail.com>
2014-07-22 23:24:31 +0800

    Update lsd2_example.m

--- matlab/x86/lsd2_example.m
+++ matlab/x86/lsd2_example.m
@@ -2,5 +2,6 @@
 im = imread('./images/test.jpg');
 imshow(im);
 %% show the binary image after the process of LSD.
+% note: input parameter is the path of image, use '/' as file separator.
 figure;
 imshow(lsd2('./images/test.jpg'));
