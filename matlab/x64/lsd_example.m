9d4cfac58fd61533b71ffaeee0649b4b0fa5f262
primetang <tanggefu@gmail.com>
2014-07-27 21:33:53 +0800

    Update lsd_example.m

--- matlab/x64/lsd_example.m
+++ matlab/x64/lsd_example.m
@@ -9,5 +9,5 @@ points = lsd('./images/test.jpg');
 %% plot the lines.
 hold on;
 for i = 1:size(points, 2)
-    plot(points(1:2, i), points(3:4, i),'LineWidth', 2, 'Color', [1, 0, 0]);
+    plot(points(1:2, i), points(3:4, i), 'LineWidth', 2, 'Color', [1, 0, 0]);
 end
